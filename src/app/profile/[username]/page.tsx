ff2f46e43980f6c96e4e9d77ad706a0281af8241
Leonardo.nunes <leonardo.nunes@compasso.com.br>
2024-07-19 15:19:02 -0300

    fix: remove console GITHUB_TOKEN

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -7,8 +7,6 @@ import api from '@/services/api';
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 async function fetchUserData(username: string): Promise<{ user: UserProps | null, repos: RepoProps[] | null }> {
-  
-  console.log('GITHUB_TOKEN:', process.env.GITHUB_TOKEN);
 
   try {
     const userRes = await fetch(`${api.baseURL}/${username}`, {
